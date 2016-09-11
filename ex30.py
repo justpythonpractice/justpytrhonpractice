f524537af1e1d5d115ac9d859f90959d601de813
justpythonpractice <prowebstud@gmail.com>
2016-09-11 11:47:45 -0400

    commit

--- ex30.py
+++ ex30.py
@@ -1,6 +1,6 @@
 people = 30
-cars = 40
-buses = 15
+cars = 30
+buses = 30
 
 if cars > people:
     print("we should take the cars")
@@ -8,3 +8,15 @@ elif cars < people:
     print("we should not take the cars")
 else:
     print("we can't decide")
+
+if buses > cars:
+    print("That's too many buses")
+elif buses < cars:
+    print("Maybe we could take the buses")
+else:
+    print("We still can't decide!")
+
+if people > buses:
+    print("All right, let's just take the buses")
+else:
+    print("fine! let's stay at home then.")
