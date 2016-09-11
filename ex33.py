14c5761dd91a1a10d612af6d8b7bd9fa77cfc646
justpythonpractice <prowebstud@gmail.com>
2016-09-11 14:16:31 -0400

    commit

---
+++ ex33.py
@@ -0,0 +1,15 @@
+def while_loop(arg1):
+    i = 0
+    numbers = []
+    while i <= arg1:
+        print("At the top i is %d" % i)
+        numbers.append(i)
+        i = i + 1
+        print("Numbers now: ", numbers)
+    print("At the bottom i is %d" % i)
+    print("The numbers: ")
+    for num in numbers:
+        print(num)
+
+
+while_loop(13)
